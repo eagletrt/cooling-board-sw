0cbee92af4c7c4f357742f059c125f3c84718d7a
killzyy <majorfrost173@gmail.com>
2026-06-17 22:48:53 +0200

    feat: added openblt files

--- cooling-board-bootloader/Core/Src/gpio.c
+++ cooling-board-bootloader/Core/Src/gpio.c
@@ -34,13 +34,11 @@
 
 /** Pinout Configuration
 */
-void MX_GPIO_Init(void)
-{
+void MX_GPIO_Init(void) {
 
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOB_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-
+    /* GPIO Ports Clock Enable */
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
 }
 
 /* USER CODE BEGIN 2 */
