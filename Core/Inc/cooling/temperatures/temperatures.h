ffc56d2606bce79f3f57d97b6b4464d2135ca92d
killzyy <majorfrost173@gmail.com>
2026-06-22 18:05:58 +0200

    feat: added enumeration for temperature names

--- Core/Inc/cooling/temperatures/temperatures.h
+++ Core/Inc/cooling/temperatures/temperatures.h
@@ -8,8 +8,10 @@
 #ifndef TEMPERATURES_H
 #define TEMPERATURES_H
 
+#include <stddef.h>
+
 /*! \brief The number of temperatures that are read from the sensors */
-#define TEMPERATURES_COUNT (8U)
+constexpr size_t TEMPERATURES_COUNT = 8U;
 
 /*!
  * \brief Return codes for the temperatures module functions
@@ -17,7 +19,22 @@
 enum TemperaturesReturnCode {
     TEMPERATURES_RC_OK = 0,       /*!< The function executed succesfully */
     TEMPERATURES_RC_NULL_POINTER, /*!< An argument of pointer type was null or a null pointer was used inside the function */
+    TEMPERATURES_RC_INVALID_NAME, /*!< The name of the temperature that was provided is not valid */
     TEMPERATURES_RC_ERROR         /*!< The function encountered an error */
+};
+
+/*!
+ * \brief Names for the temperatures read, one from each of the sensors
+ */
+enum TemperaturesName {
+    TEMPERATURES_RIGHT_PUMP_EXIT_TEMPERATURE,
+    TEMPERATURES_FRONT_RIGHT_MOTOR_EXIT_TEMPERATURE,
+    TEMPERATURES_REAR_RIGHT_MOTOR_EXIT_TEMPERATURE,
+    TEMPERATURES_RIGHT_RADIATOR_EXIT_TEMPERATURE,
+    TEMPERATURES_LEFT_PUMP_EXIT_TEMPERATURE,
+    TEMPERATURES_FRONT_LEFT_MOTOR_EXIT_TEMPERATURE,
+    TEMPERATURES_REAR_LEFT_MOTOR_EXIT_TEMPERATURE,
+    TEMPERATURES_LEFT_RADIATOR_EXIT_TEMPERATURE
 };
 
 /*!
