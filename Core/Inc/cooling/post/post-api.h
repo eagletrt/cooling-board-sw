4c31ada380149b23fa27ec8695289edfb08d1f01
killzyy <majorfrost173@gmail.com>
2026-06-28 19:13:03 +0200

    feat: added temperatures modules initalization in POST

--- Core/Inc/cooling/post/post-api.h
+++ Core/Inc/cooling/post/post-api.h
@@ -14,7 +14,16 @@
  * \brief Run the power-on self-test
  *
  * \retval POST_RC_OK if POST has been able to complete succesfully
+ * \retval POST_RC_ERROR if POST has been unsuccessful
  */
 enum PostReturnCode post_api_run(void);
+
+/*!
+ * \brief Initializes all the modules
+ *
+ * \retval POST_RC_OK if all the modules have been initialized succesfully
+ * \retval POST_RC_ERROR if any module was not able to initialize succesfully
+ */
+enum PostReturnCode post_api_modules_init(void);
 
 #endif // POST_API_H
