2a7e05db7fad843243498f9e468250f15e546ab7
killzyy <majorfrost173@gmail.com>
2026-07-25 19:04:19 +0200

    feat: added test for temperatures module initialization

--- Core/Test/test_temperatures/test_temperatures.c
+++ Core/Test/test_temperatures/test_temperatures.c
@@ -7,7 +7,16 @@ extern struct TemperaturesHandler temperatures_handler;
 constexpr int TEMPERATURES_NAME_INVALID_NAME = -1;
 
 void setUp(void) {
-    memset(&temperatures_handler, 0, sizeof(temperatures_handler));
+    memset(&temperatures_handler, 0U, sizeof(temperatures_handler));
+}
+
+void test_temperatures_initialization_to_zero(void) {
+    struct TemperaturesHandler dummy_zero_handler = { 0 };
+
+    memset(&temperatures_handler, 0xFF, sizeof(temperatures_handler));
+
+    TEST_ASSERT_EQUAL(TEMPERATURES_RC_OK, temperatures_api_init());
+    TEST_ASSERT_EQUAL_MEMORY(&dummy_zero_handler, &temperatures_handler, sizeof(temperatures_handler));
 }
 
 void test_temperatures_get_temperature_invalid_name(void) {
@@ -34,6 +43,8 @@ void test_temperatures_set_temperature_valid_name(void) {
 
 int main(void) {
     UNITY_BEGIN();
+
+    RUN_TEST(test_temperatures_initialization_to_zero);
 
     RUN_TEST(test_temperatures_get_temperature_invalid_name);
     RUN_TEST(test_temperatures_get_temperature_valid_name);
