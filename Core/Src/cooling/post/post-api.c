bd74419cbf1b715e5b315a9e4317420e8fc3b6a0
killzyy <majorfrost173@gmail.com>
2026-07-27 18:19:07 +0200

    fix: cleared messy post module code

--- Core/Src/cooling/post/post-api.c
+++ Core/Src/cooling/post/post-api.c
@@ -10,10 +10,6 @@
 #include "can-communication-api.h"
 
 enum PostReturnCode post_api_run(struct PostInitData *post_init_data) {
-    if (temperatures_api_init() != TEMPERATURES_RC_OK) {
-        return POST_RC_UNINITIALIZED_MODULE;
-    }
-
     if (post_init_data == nullptr ||
         post_init_data->can_network_configurations[CAN_COMMUNICATION_NETWORK_PRIMARY].on_receive == nullptr ||
         post_init_data->can_network_configurations[CAN_COMMUNICATION_NETWORK_PRIMARY].send == nullptr) {
@@ -21,6 +17,10 @@ enum PostReturnCode post_api_run(struct PostInitData *post_init_data) {
     }
 
     enum PostReturnCode post_return_code = POST_RC_OK;
+
+    if (temperatures_api_init() != TEMPERATURES_RC_OK) {
+        return POST_RC_UNINITIALIZED_MODULE;
+    }
 
     if (can_communication_api_init(post_init_data->can_network_configurations) != CAN_COMMUNICATION_RC_OK) {
         post_return_code = POST_RC_UNINITIALIZED_MODULE;
