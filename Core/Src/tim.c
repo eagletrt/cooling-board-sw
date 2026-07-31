01017605073c6d61ce57c258a31c3803675f12a4
killzyy <majorfrost173@gmail.com>
2026-07-31 15:45:43 +0200

    chore: updated user label for control pins

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -158,19 +158,19 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef *timHandle) {
     PB7     ------> TIM3_CH4
     PB8     ------> TIM3_CH1
     */
-        GPIO_InitStruct.Pin = GPIO_PIN_5 | GPIO_PIN_7 | GPIO_PIN_8;
+        GPIO_InitStruct.Pin = PUMP_L_CMD_Pin | FAN_L_CMD_Pin | PUMP_R_CMD_Pin;
         GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
         GPIO_InitStruct.Pull = GPIO_NOPULL;
         GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
         GPIO_InitStruct.Alternate = GPIO_AF3_TIM3;
         HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-        GPIO_InitStruct.Pin = GPIO_PIN_6;
+        GPIO_InitStruct.Pin = FAN_R_CMD_Pin;
         GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
         GPIO_InitStruct.Pull = GPIO_NOPULL;
         GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
         GPIO_InitStruct.Alternate = GPIO_AF13_TIM3;
-        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+        HAL_GPIO_Init(FAN_R_CMD_GPIO_Port, &GPIO_InitStruct);
 
         /* USER CODE BEGIN TIM3_MspPostInit 1 */
 
